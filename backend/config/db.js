043a694e4a27298cee8f789115c12e3267aa3ef2
Ayodhya Mahesh Babu <142784557+ayodhya16@users.noreply.github.com>
2025-09-18 22:57:28 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://maheshbabuayodhya_db_user:babumb1608@cluster0.npzkrkj.mongodb.net/TaskFlow')
+    await mongoose.connect('"monogb credentials')
     .then(() => console.log("DB connected"))
 }
